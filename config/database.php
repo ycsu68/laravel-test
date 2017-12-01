d46621197ad159de1e86fd928673854820207a1d
YC SU <yc.su@jetfuel.com.tw>
2017-12-01 16:51:08 +0800

    test

--- config/database.php
+++ config/database.php
@@ -39,13 +39,34 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        /*'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],*/
+        'mysql' => [
+            'read' => [
+                'host' => env('DB_HOST_READ', '127.0.0.1'),
+                'username' => env('DB_USERNAME_READ', 'forge'),
+                'password' => env('DB_PASSWORD_READ', ''),
+            ],
+            'write' => [
+                'host' => env('DB_HOST_WRITE', '127.0.0.1'),
+                'username' => env('DB_USERNAME_WRITE', 'forge'),
+                'password' => env('DB_PASSWORD_WRITE', ''),
+            ],
+            'driver' => 'mysql',
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
