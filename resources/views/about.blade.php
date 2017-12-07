998aa919e0172cca7378cd60ba3c6711c7b33f80
YC SU <yc.su@jetfuel.com.tw>
2017-12-07 16:07:25 +0800

    auth template

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('title', 'About us')
+
+@section('content')
+    <header class="headline">
+        <h1 class="headline-title">About us</h1>
+        <p class="headline-text">This is a really cool application demonstrating some core features of Laravel.</p>
+    </header>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('foo', function () {
-    return 'Hello World';
+
+Route::get('about', function() {
+	return view('about');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
